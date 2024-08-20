edc7cd4cf790f6de0c0036bb41b6bc1cbdad38e3
john <jayachandrag69@gmail.com>
2024-08-20 13:53:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizapplication/Splash_Screen.java
@@ -0,0 +1,55 @@
+package com.example.quizapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash_Screen extends AppCompatActivity {
+
+
+    ImageView image;
+    TextView text;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        image = findViewById(R.id.imageSplash);
+        text = findViewById(R.id.textSplash);
+
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splash_anim);
+        text.startAnimation(animation);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(Splash_Screen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        },5000);
+
+    }
+}
